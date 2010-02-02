6464567412f1e4f5d20576b3173cbdaeea885835
dbetz <>
2010-02-02 00:41:50 -0600

    Cleaned up.

--- IHasPage.cs
+++ IHasPage.cs
@@ -2,7 +2,6 @@
 //+ Nalarium Pro 3.0 - Web Module
 //+ Copyright © Jampad Technology, Inc. 2008-2010
 #endregion
-using System;
 //+
 //+
 namespace Nalarium.Web
--- Resource.Designer.cs
+++ Resource.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Nalarium.Web {
-    using System;
-    
-    
+namespace Nalarium.Web
+{
+
+
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
